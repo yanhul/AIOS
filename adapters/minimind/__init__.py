19cd8e76a78fe00513f1a4c96b0512e443f9f562
yanhul <116991963+yanhul@users.noreply.github.com>
2026-09-07 08:07:11 +0700

    feat: add MiniMind adapter package

---
+++ adapters/minimind/__init__.py
@@ -0,0 +1,5 @@
+"""Governed boundary for the external MiniMind learning workload."""
+
+from .contract import MINIMIND_CAPABILITY, validate_receipt
+
+__all__ = ["MINIMIND_CAPABILITY", "validate_receipt"]
