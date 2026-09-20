f08e5f2733c32600a60d1bc08af7e63c1ee8b974
yanhul <116991963+yanhul@users.noreply.github.com>
2026-09-20 21:09:27 +0700

    test: keep G4A assertion source-independent

--- tests/test_g4a_fixture.py
+++ tests/test_g4a_fixture.py
@@ -1,5 +1,5 @@
-from scripts.g4a_fixture import VALUE
+import scripts.g4a_fixture
 
 
-def test_g4a_fixture_is_repaired():
-    assert VALUE == "FIXED"
+def test_g4a_fixture_imports_after_repair():
+    assert scripts.g4a_fixture.VALUE == "FIXED"
